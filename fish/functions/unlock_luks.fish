f78f057118f68a4459de5a3267272d70a1b76b02
DresenDominique <dominique.dresen@bsi.bund.de>
2025-04-11 22:37:33 +0200

    Add script to quick connect to VPN with keepass, analogue to luks unlock

--- fish/functions/unlock_luks.fish
+++ fish/functions/unlock_luks.fish
@@ -13,6 +13,8 @@ function unlock_luks --description 'Unlock an encrypted LUKS device using secret
         echo "❌ Could not get LUKS UUID for $device"
         return 1
     end
+
+    echo "Checking passphrase for UID $uuid"
     
     # Get passphrase from Secret Service
     set pass (secret-tool lookup filesystem luks luks.uuid $uuid)
@@ -21,12 +23,24 @@ function unlock_luks --description 'Unlock an encrypted LUKS device using secret
         echo "❌ Could not retrieve passphrase from secret agent"
         return 1
     end
+    echo "Got the password. Going to unlock and mount"
     
-    # Use expect to feed the password to udisksctl
-    expect -c "
+    set output (expect -c "
+        log_user 1
         spawn udisksctl unlock -b $device
         expect \"Passphrase:\"
         send \"$pass\r\"
         expect eof
-    "
+    ")
+
+    # Extract the mapped device from the captured output
+    set mapped (echo $output | string match -r '/dev/dm-[0-9]+')
+
+    echo "Now only need to mount $mapped"
+    # Check if unlock succeeded and mount it
+    if test -n "$mapped"
+        udisksctl mount -b $mapped
+    else
+        echo "❌ Failed to unlock $device"
+    end
 end
