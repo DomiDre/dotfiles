ddc9d3de717c392a7e2f7891d4c2b4b79c8267bc
DomiDre <DomiDre@users.noreply.github.com>
2025-08-19 17:01:32 +0200

    Adjust fix_display script

--- fish/functions/fix_display.fish
+++ fish/functions/fix_display.fish
@@ -1,4 +1,3 @@
 function fix_display --wraps='DISPLAY=:0 xrandr --output eDP-1 --auto' --description 'Set display to laptop'
-  DISPLAY=:0 xrandr --output eDP-1 --auto $argv
-        
+  xrandr --output HDMI-0 --mode 7680x2160
 end
